6408137ab30cb41f8bacd5c2164fb514bbfe92db
Kyle Ledin <Kyle.Ledin85@gmail.com>
2023-02-28 21:28:38 -0600

    adds more bootstrap styling to the index views and adds a nav bar

--- app/facades/trail_facade.rb
+++ app/facades/trail_facade.rb
@@ -1,14 +1,19 @@
 class TrailFacade
   def self.search_trails(uri)
+    binding.pry
     @trails = filter_trails(uri)[:data].map do |trail|
       Trail.new(trail)
     end
   end
 
   def self.show_trail(uri)
-    @trail = filter_trails(uri)[:data].map do |trail|
-      Trail.new(trail)
+    trail = Trail.new(filter_trails(uri)[:data][:trail])
+    breweries_data = filter_trails(uri)[:data][:breweries]
+    breweries = breweries_data.map do |brewery| 
+      Brewery.new(brewery)
     end
+    trail_breweries = {trail => breweries}
+    # binding.pry
   end
 
   def self.filter_trails(uri)
