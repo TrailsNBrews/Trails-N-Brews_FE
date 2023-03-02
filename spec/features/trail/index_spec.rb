665a9596d277a63226a32e2d0c3e4dc18ae9b015
cemccabe <46434166+cemccabe@users.noreply.github.com>
2023-03-01 23:22:12 -0700

    Modify fixture files and stub requests

--- spec/features/trail/index_spec.rb
+++ spec/features/trail/index_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe 'trails index' do
   it 'has a search bar to search trails' do
-    json_response = File.read('spec/fixtures/buffalo-trails_correct.json')
+    json_response = File.read('spec/fixtures/buffalo_trails.json')
     stub_request(:get, "https://evening-caverns-30828.herokuapp.com/api/v1/search_trails?search=Buffalo")
       .to_return(status: 200, body: json_response)
 
@@ -11,10 +11,12 @@ RSpec.describe 'trails index' do
     click_button 'Find Trails'
 
     expect(page).to have_content("Buffalo (Colorado Trail) Trailhead")
+    expect(page).to have_content("Buffalo Ridge Trailhead")
+    expect(page).to have_content("Buffalo Pass Trailhead")
   end
 
   it 'can take me to a trails show page' do
-    json_response = File.read('spec/fixtures/buffalo-trails_correct.json')
+    json_response = File.read('spec/fixtures/buffalo_trails.json')
     stub_request(:get, "https://evening-caverns-30828.herokuapp.com/api/v1/search_trails?search=Buffalo")
       .to_return(status: 200, body: json_response)
       
@@ -22,7 +24,7 @@ RSpec.describe 'trails index' do
     fill_in(:name, with: "Buffalo")
     click_button 'Find Trails'
 
-    json_response = File.read('spec/fixtures/buffalo_trailhead_correct.json')
+    json_response = File.read('spec/fixtures/buffalo_trailhead_63243.json')
     stub_request(:get, "https://evening-caverns-30828.herokuapp.com/api/v1/search_trails/63243")
       .to_return(status: 200, body: json_response)
 
