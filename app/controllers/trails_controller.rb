d77ab95513cf0a54d9b519213c392e301543ef18
Leo Banos Garcia <bsleo625@gmail.com>
2023-03-02 16:12:35 -0700

    Fix when a trail search results with no results it won't try to run Trail.new in facade

--- app/controllers/trails_controller.rb
+++ app/controllers/trails_controller.rb
@@ -1,11 +1,11 @@
 class TrailsController < ApplicationController
   def index
-      if params[:name]
-        @trails = TrailFacade.search_trails("/api/v1/search_trails?search=#{params[:name]}")
-          if @trails == []
-              flash.alert = "Search returned no results"
-          end
-      end
+    if params[:name]
+      @trails = TrailFacade.search_trails("/api/v1/search_trails?search=#{params[:name]}")
+        if @trails == []
+            flash.alert = "Search returned no results"
+        end
+    end
   end
 
   def show
