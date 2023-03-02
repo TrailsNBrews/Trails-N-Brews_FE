750d4e57920079df4beeb9e88919f28840416ff4
Leo Banos Garcia <bsleo625@gmail.com>
2023-03-02 11:49:37 -0700

    Adds fixture for empty results and flash alert to application layout in views

--- spec/features/brewery/index_spec.rb
+++ spec/features/brewery/index_spec.rb
@@ -37,28 +37,27 @@ RSpec.describe "brewery index" do
     expect(current_path).to eq(brewery_path("10-barrel-brewing-co-denver-denver"))
   end
   
-  # describe 'sad path testing' do
-  #   it 'returns an error when no results match' do
-  #     json_response = File.read('spec/fixtures/colorado_breweries.json')
-  #     stub_request(:get, "https://evening-caverns-30828.herokuapp.com/api/v1/search_breweries?name=cheese").
-  #        with(
-  #          headers: {
-  #      	  'Accept'=>'*/*',
-  #      	  'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
-  #      	  'User-Agent'=>'Faraday v2.7.4'
-  #          }).
-  #        to_return(status: 200, body: "", headers: {})
-  # 
-  #       visit breweries_path
-  #       fill_in(:name, with: "cheese")
-  #       click_button 'Find Brewery'
-  # 
-  #       # save_and_open_page
-  # 
-  #       expect(current_path).to eq(breweries_path)
-  #       expect(page).to have_content("Search returned no results")
-  #   end
-  # end
+  describe 'sad path testing' do
+    it 'returns an error when no results match' do
+      json_response = File.read('spec/fixtures/no_brewery_results.json')
+      
+      stub_request(:get, "https://evening-caverns-30828.herokuapp.com/api/v1/search_breweries?name=cheese").
+         with(
+           headers: {
+       	  'Accept'=>'*/*',
+       	  'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+       	  'User-Agent'=>'Faraday v2.7.4'
+           }).
+         to_return(status: 200, body: json_response, headers: {})
+  
+        visit breweries_path
+        fill_in(:name, with: "cheese")
+        click_button 'Find Brewery'
+    
+        expect(current_path).to eq(breweries_path)
+        expect(page).to have_content("Search returned no results")
+    end
+  end
   # 
   # describe 'user create or find' do
   #   it 'can create or find a user'  do
