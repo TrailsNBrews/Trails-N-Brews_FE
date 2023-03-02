3d395e3ca5b081fb8eeb7e3cb8c86592a3d4789b
Leo Banos Garcia <bsleo625@gmail.com>
2023-03-01 17:26:37 -0700

    Fix separates coordinate attribute in trail poro to latitude and longitude

--- app/poros/trail.rb
+++ app/poros/trail.rb
@@ -2,14 +2,16 @@ class Trail
   attr_reader :id,
               :type,
               :name,
-              :coordinates,
+              :latitude,
+              :longitude,
               :bathrooms
 
   def initialize(data)
     @id = data[:id]
     @type =data[:type]
     @name = data[:attributes][:name]
-    @coordinates = data[:attributes][:coordinates]
+    @latitude = data[:attributes][:latitude]
+    @longitude = data[:attributes][:longitude]
     @bathrooms = data[:attributes][:bathrooms]
   end
 end
