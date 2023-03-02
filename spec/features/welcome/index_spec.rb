a9af0f323315f73b7b775a32f3910645b60e99bb
cemccabe <46434166+cemccabe@users.noreply.github.com>
2023-03-02 12:50:16 -0700

    Update landing page with background image, remove old test from index spec

--- spec/features/welcome/index_spec.rb
+++ spec/features/welcome/index_spec.rb
@@ -6,12 +6,4 @@ RSpec.describe 'Welcome page' do
     expect(current_path).to eq(root_path)
     expect(page).to have_content("Trails N' Brews")
   end
-
-  # it 'takes us to the Create New User page' do
-  #   visit root_path
-
-  #   click_button 'New User'
-
-  #   expect(current_path).to eq(new_user_path)
-  # end
 end
